50c5cf7d3fc430c414906c554b99b88115301b2c
charchit95 <charchit95@gmail.com>
2021-07-30 23:03:37 +0530

    Styles Updated

--- src/App.js
+++ src/App.js
@@ -20,47 +20,24 @@ function App() {
 
   let gameLogin = (board, player, row, col) => {
     let rCount = 0;
+    let cCount = 0;
+    let lCount = 0;
+    let drCount = 0;
     for (let r = 0; r < 3; r++) {
-      if(board[r][col] === player) {
+      if (board[r][col] === player) {
         rCount += 1;
       }
-    }
-    if (rCount === 3) {
-      return true;
-    }
-
-    let cCount = 0;
-    for (let c = 0; c < 3; c++) {
-      if(board[row][c] === player) {
+      if (board[row][r] === player) {
         cCount += 1;
       }
-    }
-    if (cCount === 3) {
-      return true;
-    }
-    
-    let lCount = 0;
-    for (let l = 0; l < 3; l++) {
-      if(board[l][l] === player) {
+      if (board[r][r] === player) {
         lCount += 1;
       }
-    }
-    if (lCount === 3) {
-      return true;
-    }
-    
-    let drCount = 0;
-    for (let dr = 0; dr < 3; dr++) {
-      if(board[dr][2-dr] === player) {
+      if (board[r][2 - r] === player) {
         drCount += 1;
       }
     }
-    if (drCount === 3) {
-      return true;
-    }
-
-    return false;    
-
+    return (rCount === 3 || cCount === 3 || lCount === 3 || drCount === 3) ? true : false;
   }
 
   return (
@@ -70,35 +47,32 @@ function App() {
           <div className="ticRow" key={i}>
             {row.map((item, cindex) => {
               return (
-                <button 
-                  key={cindex} 
+                <button
+                  key={cindex}
                   className="tictacbtn"
                   disabled={boardState.gameOver}
                   onClick={() => {
-                    let copyState = {...boardState}
+                    let copyState = { ...boardState }
                     if (copyState.board[i][cindex] === 0) {
                       copyState.board[i][cindex] = copyState.player;
                       let result = gameLogin(copyState.board, copyState.player, i, cindex)
-                      
-                      if (result){
+                      if (result) {
                         copyState.gameOver = true;
                         copyState.winner = copyState.player;
                       } else {
                         copyState.winner = 0;
                       }
-                      copyState.steps +=1;
-                      
+                      copyState.steps += 1;
                       if (copyState.steps === 9) {
                         copyState.gameOver = true;
                         // copyState.winner = 0;
                       }
-                      
                       copyState.player = -copyState.player;
                       setBoardState(copyState)
                     }
                   }}
                 >
-                  {item === 0 ? undefined : item}
+                  {item === 0 ? undefined : item === 1 ? "O" : "X"}
                 </button>
               )
             })
@@ -107,16 +81,17 @@ function App() {
           </div>
         )
       })}
+
       {!boardState.gameOver &&
 
-      <h1>Current Move: Player {boardState.player}</h1>
+        <h1>Current Move: Player {boardState.player === 1 ? "O" : "X"}</h1>
       }
       {boardState.gameOver && (
         <div>
-          <h1>Winner is {boardState.winner}</h1>
+          <h1>Winner is {boardState.winner === 1 ? "O" : "X"}</h1>
         </div>
       )}
-      <button onClick={() => setBoardState(initial)} >Reset</button>
+      <button onClick={() => setBoardState(initial)} className="reset" >Reset</button>
     </div>
   );
 }
